9d925ce9a13641bef50cf0aa55610ffbb73466a9
utmp <2210206543@ogrenci.karabuk.edu.tr>
2025-04-12 16:45:30 +0300

    Added tailwindcss Added daisyUI component library

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/renderer/index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/**/*.{vue,js,ts,jsx,tsx,svg}",
   ],
   theme: {
     extend: {},
