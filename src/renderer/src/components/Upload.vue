68cdb2b9bdb16ba565ee5189b6367ddaefa9e1bd
utmp <2210206543@ogrenci.karabuk.edu.tr>
2025-04-12 16:47:07 +0300

    updated

--- src/renderer/src/components/Upload.vue
+++ src/renderer/src/components/Upload.vue
@@ -5,7 +5,7 @@ function onFilePicked (e){
     console.log(files)
 }
 onMounted(()=>{
-    const dropArea = document.querySelector('.drop-area');
+    const dropArea = document.querySelector('label');
     const folderIcon = dropArea.querySelector('img')
     const defPath = folderIcon.src;
     dropArea.addEventListener('mouseover',(e)=>{
@@ -19,9 +19,9 @@ onMounted(()=>{
 </script>
 <template>
 <div class="drop-area icons flex items-center justify-center w-full h-full">
-    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-full border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+    <label for="dropzone-file" class="flex flex-col items-center justify-center w-9/12 h-6/12 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
         <div class="flex flex-col items-center justify-center pt-5 pb-6">
-           <img src="../assets/folder.svg" alt="">
+           <img src="../assets/folder.svg" alt="" class="size-24">
             <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
         </div>
         <input id="dropzone-file" type="file" class="hidden" @change="onFilePicked"/>
