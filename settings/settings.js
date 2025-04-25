99ee7250052989b1d0b347aab9d87ee00e14f2ea
utmp <2210206543@ogrenci.karabuk.edu.tr>
2025-04-25 13:50:00 +0300

    Deleted unnecessary files from old workspace

--- settings/settings.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('api', {
-    openDirectory: () => ipcRenderer.invoke('dialog:openDirectory')
-});
-contextBridge.exposeInMainWorld('darkMode',{
-    toggle: ()=> ipcRenderer.invoke('dark-mode-toggle'),
-    system:()=> ipcRenderer.invoke('dark-mode-system')
-})
